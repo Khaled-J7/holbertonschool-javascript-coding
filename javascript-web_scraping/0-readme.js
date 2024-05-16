bfbacf3969fe77581d44671ba5826c7958f9f520
Khaled Jallouli <6711@holbertonstudents.com>
2024-05-16 21:28:23 +0100

    Update 0-readme.js

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-
+// a script that reads and prints the content of a file.
 const fs = require('fs');
-
-fs.readFile(process.argv[2], 'utf8', function (err, data) {
+const fileName = process.argv[2];
+fs.readFile(fileName, 'utf8', (err, data) => {
   if (err) {
-    console.log(err);
+    console.log(`Error reading file "${fileName}":`, err);
   } else {
     console.log(data);
   }
