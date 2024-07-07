63e9cec65b92362ecaeb929eb5716a2168ba680c
Khaled-J7 <jallouli.khaled.2001@gmail.com>
2024-07-08 00:13:17 +0100

    Update 0-readme

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
+
 const fs = require('fs');
 
-const filePath = process.argv[2];
-fs.readFile(filePath, 'utf-8', (error, data) => {
-  if (error) {
-    console.error(error);
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
   } else {
     console.log(data);
   }
